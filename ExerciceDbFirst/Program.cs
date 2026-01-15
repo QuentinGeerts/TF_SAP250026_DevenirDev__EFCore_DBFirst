2fed593d5b1762167781d73a13f8da9ee4664d97
Quentin Geerts <geerts.quentin@gmail.com>
2026-01-15 14:53:44 +0100

    Correction exercice sur l'approche DB First

---
+++ ExerciceDbFirst/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Exercice DBFirst - Récupération de la base de données "DBSlide"
+ * Importer la base de donnée "DBSlide" en tant que projet EFCore.
+ * Afficher la liste des étudiants ainsi que le nom de la section dans laquelle ils sont.
+ */
+
+using ExerciceDbFirst.Data;
+using Microsoft.EntityFrameworkCore;
+
+using (DbslideContext context = new DbslideContext())
+{
+    var studentsWithSectionName = context.Students;
+
+	foreach (var s in studentsWithSectionName)
+	{
+		// ...
+	}
+
+	foreach (var s in context.Students.Include(s => s.Section))
+	{
+        Console.WriteLine($"{s.LastName?.PadLeft(15, ' ')} {s.FirstName?.PadLeft(15, ' ')} {s.Section?.SectionName?.PadLeft(20, ' ')}");
+	}
+
+}
+
